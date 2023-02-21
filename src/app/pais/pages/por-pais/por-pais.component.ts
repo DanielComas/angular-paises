2ea07414536852e406e1e0a54ac96ffa99152945
DanielComas <dcomasc8810@gmail.com>
2023-02-21 17:09:39 -0500

    Terminando busqueda por pais y por region, ademas de optimizando servicios

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -6,6 +6,11 @@ import {Country} from "../../interfaces/pais-interfaces";
   selector: 'app-por-pais',
   templateUrl: './por-pais.component.html',
   styles: [
+    `
+        li {
+          cursor: pointer;
+        }
+    `
   ]
 })
 export class PorPaisComponent {
@@ -14,18 +19,22 @@ export class PorPaisComponent {
   hayError: boolean = false;
   paises: Country[] = [];
 
+  paisesSugeridos: Country[] = [];
+  mostrarSugerencia: boolean = false;
+
   constructor(private paisService: PaisService) {
   }
 
   buscar( termino: string) {
     this.hayError = false;
     this.termino = termino;
+    this.mostrarSugerencia = false;
 
     this.paisService.buscarPais(termino)
       .subscribe(paises => {
         console.log(paises);
         this.paises = paises;
-      }, (err) => {
+      }, () => {
         this.hayError = true;
         this.paises = [];
       });
@@ -33,7 +42,14 @@ export class PorPaisComponent {
 
   sugerencias( termino: string) {
     this.hayError = false;
-    //TODO: crear sugerencias
+    this.termino = termino;
+    this.mostrarSugerencia = true;
+
+    this.paisService.buscarPais(termino)
+      .subscribe(
+        paises => this.paisesSugeridos = paises.splice(0, 5),
+          () => this.paisesSugeridos = [] );
+
   }
 
 }
--- src/app/pais/pages/por-region/por-region.component.ts
+++ src/app/pais/pages/por-region/por-region.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Country} from "../../interfaces/pais-interfaces";
+import {PaisService} from "../../services/pais.service";
 
 @Component({
   selector: 'app-por-region',
@@ -8,4 +10,32 @@ import { Component } from '@angular/core';
 })
 export class PorRegionComponent {
 
+
+  regiones: string[] = ['EU', 'EFTA', 'CARICOM', 'PA', 'AU', 'USAN', 'EEU', 'AL', 'ASEAN', 'CAIS', 'CEFTA', 'NAFTA', 'SAARC'];
+
+  regionActiva: string = '';
+
+  paises: Country[] = [];
+
+  constructor(private paisService: PaisService) {
+  }
+
+  getClase(region: string): string {
+     return (region === this.regionActiva)
+       ? 'btn btn-primary mr-1'
+       : 'btn btn-outline-primary mr-1';
+  }
+
+  activarRegion(region: string) {
+
+    if (region === this.regionActiva) { return }
+    this.regionActiva = region;
+    this.paises = [];
+
+    this.paisService.buscarRegion(region).subscribe( paises => {
+      this.paises = paises;
+    }, () => {
+      this.paises = [];
+    });
+  }
 }
